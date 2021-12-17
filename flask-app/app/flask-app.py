47e97caa70e62400e0e76f436c9f3fc0eca52646
Oketunji Oludolapo <85907290+OketunjiOludolapo@users.noreply.github.com>
2021-12-17 16:12:22 +0100

    assignment2

--- flask-app/app/flask-app.py
+++ flask-app/app/flask-app.py
@@ -6,7 +6,7 @@ app=Flask("__name__")
 @app.route('/',methods=["GET","POST"])
 
 def get_input():
-
+    """A function to send request through Flask, evaluate the request and provide the result"""
     packet=request.get_json(force=True)
     height=packet["height"]
     weight=packet["weight"]
